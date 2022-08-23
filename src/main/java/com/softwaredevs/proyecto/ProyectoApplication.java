49cf8b2e0e0381e9963429d38e34caf811827d4c
alexc9805 <brayanc98@gmail.com>
2022-08-22 21:01:10 -0500

    Modificacion creacion proyecto

---
+++ src/main/java/com/softwaredevs/proyecto/ProyectoApplication.java
@@ -0,0 +1,14 @@
+package com.softwaredevs.proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication
+public class ProyectoApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Creacion proyecto Spring boot");
+		//SpringApplication.run(ProyectoApplication.class, args);
+	}
+
+}
