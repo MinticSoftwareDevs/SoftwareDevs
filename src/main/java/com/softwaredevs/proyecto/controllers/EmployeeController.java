05bd6991f8a2f064de8318ce10f2bd73c6202e9f
AldoAnaya <aldairanayap29@gmail.com>
2022-09-08 21:10:06 -0500

    Entity Employee and Repository - Aldair Anaya

---
+++ src/main/java/com/softwaredevs/proyecto/controllers/EmployeeController.java
@@ -0,0 +1,48 @@
+package com.softwaredevs.proyecto.controllers;
+
+import com.softwaredevs.proyecto.entities.Employee;
+import com.softwaredevs.proyecto.entities.Enterprise;
+import com.softwaredevs.proyecto.services.EnterpriseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.ldap.embedded.EmbeddedLdapAutoConfiguration;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class EmployeeController {
+    // ATRIBUTOS
+
+    EmployeeService employeeService;
+
+
+    @Autowired
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/employee")
+    public List<Employee> getEmployee() {
+        return this.employeeService.getEmployeeList();
+    }
+
+    @GetMapping("/employee/{id}")
+    public Employee getEmployee(@PathVariable("id") Long id) {
+        return this.employeeService.getEmployeeId(id);
+    }
+
+    @PostMapping("/employee")
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return this.employeeService.createEmployee(employee);
+    }
+
+    @DeleteMapping("/employee/{id}")
+    public List<Employee> removeEmployee(@PathVariable("id") Long id) {
+        return this.employeeService.removeEmployee(id);
+    }
+
+    @PatchMapping("/employee/{id}")
+    public Employee modifyEmployee(@PathVariable("id") Long id, @RequestBody Employee employee) {
+        return this.employeeService.modifyEmployee(id, employee);
+    }
+}
---
+++ src/main/java/com/softwaredevs/proyecto/repositories/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package com.softwaredevs.proyecto.repositories;
+
+import com.softwaredevs.proyecto.entities.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+}
